67129a48c9194bbd4609a3d487aa1885b3d5e89e
ilya <zapaska96@gmail.com>
2023-05-25 16:56:05 +0500

    auth

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+// import { authMiddleware } from "@clerk/nextjs";
+
+// export default authMiddleware();
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
+
+// Stop Middleware running on static files
+export const config = {
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
